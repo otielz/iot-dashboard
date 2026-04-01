89c2a59256c4147d694e12365f8de8e44fb7717c
otielz <thalielmaia@gmail.com>
2026-03-31 21:05:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-url = "https://script.google.com/macros/s/AKfycbyGiCESu5WEgq8PYbEezEoUMaXnDzatD_OVEj4DPubVj2KYE0xgnTxTX-1QyoI5VzVH/exec"
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTce46en4oO80s4P2Ep0yGCidjz3S5Ys6ibUtMAPArqRQy5-T-z6jor_Oi3Isci1ErBad3tHEalGwX1/pub?gid=0&single=true&output=csv"
 df = pd.read_csv(url)
 st.title("Monitoramento IoT")
 st.line_chart(df[['temperatura', 'umidade']])
